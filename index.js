d01e95c8086910ccc46e8a7bd6802b2e6069a5a5
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-22 23:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   for (let i = 0; i < rankedListValues.length; i++) {
    let specificValues = rankedListValues[i].children 
   
-   for (let j = 0; j < specificValues.length; J++) {
+   for (let j = 0; j < specificValues.length; j++) {
      specificValues[j].innerHTML = specificValues[j] + n
    }
   }
