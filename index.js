dab198944ab5236aeea3d47b21f0cbe7be247762
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-22 23:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function increaseRankBy(n) {
   var rankedListValues = document.querySelectorAll('.ranked-list');
   for (let i = 0; i < rankedListValues.length; i++) {
    let specificValues = rankedListValues[i].children 
-  }
+  
    for (let j = 0; j < specificValues.length; J++) {
      specificValues[j].innerHTML = specificValues[j] + n
    }
-  
+  }
 }
